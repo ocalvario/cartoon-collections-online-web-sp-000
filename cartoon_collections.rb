c6cc452374448a8e903ccaddaa8c2cbff3dca26b
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-19 20:02:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,8 @@ end
 
 roll_call_dwarves(dwarves)
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+  
 end
 
 def long_planeteer_calls# code an argument here
