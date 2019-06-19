8ea2cdd0da28eaefd0caf4bfb78d5ba82c20e66a
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-19 20:10:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,6 +30,8 @@ end
 
 long_planeteer_calls(planeteer_calls)
 
+potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
+
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
